ff9d9678afddee7fe50140ad1cef9d83fd06fab8
Sakshi <sakshi0317sv@gmail.com>
2023-02-03 03:15:03 +0530

    Second Commit

--- Mobile Manufacturer Data Analysis- SQL1.sql
+++ Mobile Manufacturer Data Analysis- SQL1.sql
@@ -1,1 +1,12 @@
---
+use MOBILE_MANUFACTURER
+
+select * from DIM_CUSTOMER
+select * from DIM_DATE
+select * from DIM_LOCATION
+select * from DIM_MANUFACTURER
+select * from DIM_MODEL
+select * from FACT_TRANSACTIONS
+
+
+--Q1. List all the states in which we have customers who have bought cellphones from 2005 till today.
+ 
