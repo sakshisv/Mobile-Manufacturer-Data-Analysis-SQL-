1f7c3ba037ba33295fc2ab03814e910421d4638c
Sakshi <sakshi0317sv@gmail.com>
2023-02-03 19:03:02 +0530

    Fourth Commit

--- Mobile Manufacturer Data Analysis- SQL1.sql
+++ Mobile Manufacturer Data Analysis- SQL1.sql
@@ -18,21 +18,27 @@ where year(b.Date)>= 2005
 
 --Q2. What state in the US is buying more 'Samsung' cell phones?
 
-select top 1 a.State, sum(b.TotalPrice) Price
-from DIM_LOCATION a
-left join FACT_TRANSACTIONS b
-on a.IDLocation = b.IDLocation
-left join DIM_MODEL c
-on b.IDModel = c.IDModel
-left join DIM_MANUFACTURER d
-on c.IDManufacturer = d.IDManufacturer
+select top 1 a.State, count(*) Cell_Phones_Count from DIM_LOCATION a
+left join FACT_TRANSACTIONS b on a.IDLocation = b.IDLocation
+left join DIM_MODEL c on b.IDModel = c.IDModel
+left join DIM_MANUFACTURER d on c.IDManufacturer = d.IDManufacturer
 where a.Country = 'US' and d.Manufacturer_Name = 'Samsung'
 group by a.State, b.TotalPrice
-order by b.TotalPrice desc
+order by 2 desc
 
 --Q3. Show the number of transactions for each model per zip code per state.
 
+select c.Model_Name, count(*) No_of_Transaction, a.ZipCode, a.State
+from DIM_LOCATION a
+left join FACT_TRANSACTIONS b on a.IDLocation = b.IDLocation
+left join DIM_MODEL c on b.IDModel = c.IDModel
+group by  c.Model_Name, a.ZipCode, a.State
 
 
-
+--Q4. Show the cheapest cellphone.
  
+select top 1 Model_Name, Unit_price
+from DIM_MODEL
+order by Unit_price asc
+
+
